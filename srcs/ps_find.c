6b9bdb6538d59cbf4beeea11ccc9846c6623eebc
ntan-wan <ntan-wan@AMYs-MacBook-Air.local>
2022-08-22 04:50:55 +0800

    FIXED FINE TARGET POS

--- srcs/ps_find.c
+++ srcs/ps_find.c
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 06:36:36 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/08/22 03:28:43 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/08/22 04:50:21 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,29 +46,38 @@ int	find_highest(t_stack *stack)
 
 void	find_target_pos_b(t_stack **stack_a, t_stack **stack_b, int max_index)
 {
-	int	i;
+	int	target_index;
 	t_stack *ptr_a;
 	t_stack *ptr_b;
 
+	(void)max_index;
 	ptr_b = *stack_b;
+	target_index = INT_MAX;
 	init_pos(*stack_a);
 	while (ptr_b)
 	{
-		i = 1;
 		ptr_a = NULL;
 		while (!ptr_a)
 		{
 			ptr_a = *stack_a;
-			while (ptr_a && ptr_a->index - ptr_b->index != i)
+			while (ptr_a)
+			{
+				if (ptr_a->index > ptr_b->index && ptr_a->index < target_index)
+					target_index = ptr_a->index;
 				ptr_a = ptr_a->next;
-			if (ptr_b->index == max_index)
+			}
+			if (target_index != INT_MAX)
+			{
+				ptr_a = *stack_a;
+				while (ptr_a && ptr_a->index != target_index) 
+					ptr_a = ptr_a->next;
+			}	
+			else
 			{
 				ptr_a = *stack_a;
 				while (ptr_a->index != find_lowest(*stack_a))
 					ptr_a = ptr_a->next;
 			}
-			else
-				i++;
 		}
 		ptr_b->target_pos = ptr_a->pos;
 		ptr_b = ptr_b->next;
