8f380062f2721a19afbf8b721c6eac8de23839f8
ntan-wan <ntan-wan@AMYs-MacBook-Air.local>
2022-09-07 02:25:15 +0800

    norminetted

--- srcs/ps_sort.c
+++ srcs/ps_sort.c
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 13:07:05 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/09/07 01:16:37 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/09/07 02:23:14 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,29 +61,3 @@ void	sort_3(t_stk **stack)
 		}
 	}
 }
-
-/*void	push_all_left_three(t_stk **stk_a, t_stk **stk_b)
-{
-	int	size;
-	int	pushed;
-
-	pushed = 0;
-	size = stack_size(*stk_a);
-	while (size > 3 && pushed < size / 2)
-	{
-		if ((*stk_a)->index < size / 2)
-		{
-			do_push(stk_a, stk_b);
-			ft_putstr_fd("pb\n", 1);
-			pushed++;
-			size--;
-		}
-		else
-			do_and_print(stk_a, do_rotate_left, "ra\n");
-	}
-	while (size-- > 3)
-	{
-		do_push(stk_a, stk_b);
-		ft_putstr_fd("pb\n", 1);
-	}
-}*/
